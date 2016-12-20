4e72b14b60383a3606ae4dadbed15cd3a8fe214a
Przemysław Wojnowski <esperanto@cumego.com>
2016-12-20 15:54:29 +0100

    Use less data in tests and separate cases.

--- test/poligon/algorithms/sorting_test.clj
+++ test/poligon/algorithms/sorting_test.clj
@@ -7,13 +7,15 @@
     (vec-swap! v 0 2)
     (is (= (persistent! v) [3 2 1 4]))))
 
-(def unsorted-data (vec (doall (repeatedly 10000 #(rand-int 10000)))))
+(def unsorted-data (vec (doall (repeatedly 10 #(rand-int 10000)))))
 
 (def sorted-data (sort unsorted-data))
 
 (deftest insertion-sort-test
-  (is (= [] (insertion-sort [])))
-  (is (= [1 2 3] (insertion-sort [1 2 3])))
-  (is (= [1 2 3 4 5] (insertion-sort [5 2 3 4 1])))
-  (is (= sorted-data (time (insertion-sort unsorted-data))))
-  (is (= sorted-data (time (insertion-sort-simple unsorted-data)))))
+  (testing "Insertion sort with immutable vectors."
+    (is (= sorted-data (insertion-sort-simple unsorted-data))))
+  (testing "Insertion sort with transient vectors."
+    (is (= [] (insertion-sort [])))
+    (is (= [1 2 3] (insertion-sort [1 2 3])))
+    (is (= [1 2 3 4 5] (insertion-sort [5 2 3 4 1])))
+    (is (= sorted-data (insertion-sort unsorted-data)))))
