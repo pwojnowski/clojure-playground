8370f8f61bd42d4ce8be39d56883615640676080
Przemysław Wojnowski <esperanto@cumego.com>
2018-02-22 10:23:23 +0100

    Use Clojure 1.9

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject poligon "0.1.0-SNAPSHOT"
   :description "Clojure playground"
   :url "http://farenda.com/clojure-tutorial"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.18.0"]]}})
