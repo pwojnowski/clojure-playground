e30edcda67eb74e7574b2eb6e66a5cad5a399409
Przemysław Wojnowski <esperanto@cumego.com>
2022-04-08 09:58:40 +0200

    Use Clojure 1.11.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject poligon "0.1.0-SNAPSHOT"
   :description "Clojure playground"
   :url "http://farenda.com/clojure-tutorial"
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
                  [criterium "0.4.5"]
                  [org.clojure/test.check "0.10.0"]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]]}})
